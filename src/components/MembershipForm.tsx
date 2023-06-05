dfc4b2d4f06c64f01939325738c5f8398ac739b6
Papapanda-PC <rafaeljonathan.work@gmail.com>
2023-06-06 00:32:11 +0700

    Popup Form init

---
+++ src/components/MembershipForm.tsx
@@ -0,0 +1,11 @@
+import { RegisterMembershipButton } from "./Buttons"
+
+const MembershipForm = () => (
+    <form>
+      <input placeholder="insert your email"></input>
+      <input placeholder="insert your firstname"></input>
+      <input placeholder="insert your lastname"></input>  
+      <RegisterMembershipButton/>
+    </form>
+)
+export default MembershipForm
---
+++ src/components/PopupMembership.tsx
@@ -0,0 +1,12 @@
+
+import MembershipForm from "./MembershipForm";
+
+const PopupMembership = () => (
+    <div>
+        <h2>Register to Photo Club Member</h2>
+        <p>Get countless benefit by joining as a member of Photo Club</p>
+        <MembershipForm />
+    </div>
+)
+
+export default PopupMembership
