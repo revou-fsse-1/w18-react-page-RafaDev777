056430ab41f6b337aea17495ed91d10b089bc867
Papapanda-PC <rafaeljonathan.work@gmail.com>
2023-06-06 00:09:36 +0700

    buttons components setup

---
+++ src/components/Buttons.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const LikeButton = () => {
+    const isLike = false;
+    isLike ? (
+    <React.Fragment>
+        <button>Like</button>
+    </React.Fragment>
+):(
+    <React.Fragment>
+        <button>Liked</button>
+    </React.Fragment>
+
+)}
+
+const JoinMembershipButton = () =>(
+    <React.Fragment>
+        <button>Join Photo Club Membership</button>
+    </React.Fragment>
+)
+
+const RegisterMembershipButton = () =>(
+        <React.Fragment>
+            <button>Register Now</button>
+        </React.Fragment>
+    )
+
+export {LikeButton, JoinMembershipButton, RegisterMembershipButton}
