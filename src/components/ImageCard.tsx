1523ac8a57837a25a2e859de580a8bae060a52fb
Papapanda-PC <rafaeljonathan.work@gmail.com>
2023-06-06 00:32:24 +0700

    iamgecard component init

---
+++ src/components/ImageCard.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+const ImageCard = (a:any) => (
+    <React.Fragment>
+        <div style=`background-image:{a.imageUrl}`>
+            <p>{a.title}</p>
+        </div>
+    </React.Fragment>
+)
