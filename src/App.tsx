2b4e4fd7f894f3f86a8fbc22988512576de930b0
PapaPanda-SwayMBP <rafaeljonathan.work@gmail.com>
2023-06-12 05:11:28 -0400

    re-init

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,6 @@ import { images as imagesData } from './datas/images';
 
 function App() {
 	const [image, setImage] = useState(imagesData);
-	const [isRegistered, setIsRegistered] = useState(false);
-	const [showForm, setShowForm] = useState(false);
-
-	const handleLiked = (id: number, isLiked: boolean) => {
-		const newLikeStatus = image.map((image) => {
-			if (image.id === id) {
-				image.isLiked = isLiked;
-			}
-			return image;
-		});
-		setImage(newLikeStatus);
-	};
-
 	const totalLikedImage = image.filter((image) => image.isLiked).length;
 	return <Fragment></Fragment>;
 }
